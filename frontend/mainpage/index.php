c348efbf682d3c2b0fc2a73d558e1166528223f2
Sagar Budhathoki <yeahme.sagar@gmail.com>
2024-02-05 09:24:04 +0545

    work on some frontend of mainpage after user login

--- frontend/mainpage/index.php
+++ frontend/mainpage/index.php
@@ -67,14 +67,14 @@
             <a class="btn">discover more</a>
         </div>
 
-        <div class="controls">
-            <span class="vid-btn active" data-src="images/vid-2.mp4"></span>
-            <span class="vid-btn" data-src="images/vid-1.mp4"></span>
-        </div>
+        <!-- <div class="controls">
+            <span class="vid-btn active" data-src="images/vid-2.mp4"></span> 
+             <span class="vid-btn" data-src="images/vid-1.mp4"></span>
+        </div> -->
 
-        <div class="video-container">
-            <video src="images/vid-2.mp4" id="video-slider" loop autoplay muted></video>
-        </div>
+        <!-- <div class="video-container">
+           <video src="images/vid-2.mp4" id="video-slider" loop autoplay muted></video> 
+        </div> -->
 
     </section>
 
