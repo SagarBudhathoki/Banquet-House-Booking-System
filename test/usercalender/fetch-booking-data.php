49182c8956a5a5328e6aa6d6447a43625af8842e
Sagar Budhathoki <yeahme.sagar@gmail.com>
2024-04-21 09:38:29 +0545

    make user booking according to calender in backend work

---
+++ test/usercalender/fetch-booking-data.php
@@ -0,0 +1,22 @@
+<?php
+require '/xampp/htdocs/Banquet=house/connection/config.php';
+// Fetch booking data from database
+$id = $_GET['page_id'];
+$result = mysqli_query($conn, "SELECT id, `booking-date` FROM reservation WHERE status='accepted' AND admin_id=$id");
+
+// Format booking data as JSON
+$data = array();
+while ($row = $result->fetch_assoc()) {
+    $event = array();
+    $event['id'] = $row['id'];
+    $event['title'] = 'Reserved';
+    $event['start'] = $row['booking-date'];
+
+    // set background color for the date
+    $event['backgroundColor'] = 'green'; // default color
+
+    // add event to data array
+    $data[] = $event;
+}
+
+echo json_encode($data);
