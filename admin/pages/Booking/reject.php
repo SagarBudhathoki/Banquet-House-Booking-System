f25eafcd3c43d56c45d73983b9d0dc76283f9158
Sagar Budhathoki <yeahme.sagar@gmail.com>
2024-04-30 15:09:19 +0545

    work on backend of new booking and reject function in admin panel

---
+++ admin/pages/Booking/reject.php
@@ -0,0 +1,14 @@
+<?php
+require '/xampp/htdocs/Banquet-house/connection/config.php';
+$admin_id = $_SESSION['admin_id'];
+
+if (!isset($admin_id)) {
+    header('location:../../../login/index.php');
+}
+$id = $_GET['id'];
+$query = "UPDATE reservation SET status ='rejected' where id=$id";
+if (mysqli_query($conn, $query)) {
+    echo "Reservation Accepted";
+} else {
+    echo "Error Occours while Accepting Reservation";
+}
